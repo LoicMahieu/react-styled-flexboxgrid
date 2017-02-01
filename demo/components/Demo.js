5935174cea9c8dd27d3e4cca4479dd5917516fed
Loïc Mahieu <mahieuloic@gmail.com>
2017-02-01 11:12:59 +0100

    demo: add code

--- demo/components/Demo.js
+++ demo/components/Demo.js
@@ -15,6 +15,20 @@ const Section = styled(p =>
   padding-top: 3rem;
 `
 
+import responsiveMD from 'html-loader!markdown-loader!../md/responsive.md'
+import offsetMD from 'html-loader!markdown-loader!../md/offset.md'
+import autoWidthMD from 'html-loader!markdown-loader!../md/auto-width.md'
+import alignStartMD from 'html-loader!markdown-loader!../md/align-start.md'
+import alignCenterMD from 'html-loader!markdown-loader!../md/align-center.md'
+import alignEndMD from 'html-loader!markdown-loader!../md/align-end.md'
+import alignTopMD from 'html-loader!markdown-loader!../md/align-top.md'
+import alignMiddleMD from 'html-loader!markdown-loader!../md/align-middle.md'
+import alignBottomMD from 'html-loader!markdown-loader!../md/align-bottom.md'
+import distAroundMD from 'html-loader!markdown-loader!../md/dist-around.md'
+import distBetweenMD from 'html-loader!markdown-loader!../md/dist-between.md'
+
+import Markdown from './Markdown'
+
 const Demo = props => (
   <div>
     <Section
@@ -33,20 +47,8 @@ const Demo = props => (
         <Box type='row' xs={10} sm={6} md={8} lg={10} />
         <Box type='row' xs={2} sm={6} md={4} lg={2} />
       </Row>
-    </Section>
-    <Section title='Fluid' description='Percent based width allow fluid resizing of columns and rows.'>
       <Row>
-        <Box type='row' xs={12} sm={3} md={2} lg={1} />
-        <Box type='row' xs={6} sm={6} md={8} lg={10} />
-        <Box type='row' xs={6} sm={3} md={2} lg={1} />
-      </Row>
-      <Row>
-        <Box type='row' xs={12} sm={3} md={2} lg={1} />
-        <Box type='row' xs={12} sm={9} md={10} lg={11} />
-      </Row>
-      <Row>
-        <Box type='row' xs={10} sm={6} md={8} lg={10} />
-        <Box type='row' xs={2} sm={6} md={4} lg={2} />
+        <Markdown html={responsiveMD} />
       </Row>
     </Section>
     <Section title='Offsets' description='Offset a column.'>
@@ -63,6 +65,9 @@ const Demo = props => (
         <Box type='row' xsOffset={2} xs={10} />
         <Box type='row' xsOffset={1} xs={11} />
       </Row>
+      <Row>
+        <Markdown html={offsetMD} />
+      </Row>
     </Section>
     <Section title='Auto Width' description='Add any number of auto sizing columns to a row. Let the grid figure it out.'>
       <Row>
@@ -74,6 +79,9 @@ const Demo = props => (
         <Box type='row' xs />
         <Box type='row' xs />
       </Row>
+      <Row>
+        <Markdown html={autoWidthMD} />
+      </Row>
     </Section>
     <Section title='Alignment' description='Add classes to align elements to the start or end of row as well as the top, bottom, or center of a column'>
       <h3><code>.start-</code></h3>
@@ -84,6 +92,9 @@ const Demo = props => (
           </Row>
         </Box>
       </Row>
+      <Row>
+        <Markdown html={alignStartMD} />
+      </Row>
       <h3><code>.center-</code></h3>
       <Row>
         <Box type='container' xs={12}>
@@ -91,6 +102,9 @@ const Demo = props => (
             <Box type='nested' xs={6} />
           </Row>
         </Box>
+      </Row>
+      <Row>
+        <Markdown html={alignCenterMD} />
       </Row>
       <h3><code>.end-</code></h3>
       <Row>
@@ -100,20 +114,32 @@ const Demo = props => (
           </Row>
         </Box>
       </Row>
+      <Row>
+        <Markdown html={alignEndMD} />
+      </Row>
       <h3><code>.top-</code></h3>
       <Row top='xs'>
         <Box type='large' xs={6} />
         <Box xs={6} />
+      </Row>
+      <Row>
+        <Markdown html={alignTopMD} />
       </Row>
       <h3><code>.middle-</code></h3>
       <Row middle='xs'>
         <Box type='large' xs={6} />
         <Box xs={6} />
       </Row>
+      <Row>
+        <Markdown html={alignMiddleMD} />
+      </Row>
       <h3><code>.bottom-</code></h3>
       <Row bottom='xs'>
         <Box type='large' xs={6} />
         <Box xs={6} />
+      </Row>
+      <Row>
+        <Markdown html={alignBottomMD} />
       </Row>
     </Section>
     <Section title='Distribution' description='Add classes to distribute the contents of a row or column.'>
@@ -127,6 +153,9 @@ const Demo = props => (
           </Row>
         </Box>
       </Row>
+      <Row>
+        <Markdown html={distAroundMD} />
+      </Row>
       <h3><code>.between-</code></h3>
       <Row>
         <Box type='container' xs={12}>
@@ -136,6 +165,9 @@ const Demo = props => (
             <Box type='nested' xs={2} />
           </Row>
         </Box>
+      </Row>
+      <Row>
+        <Markdown html={distBetweenMD} />
       </Row>
     </Section>
   </div>
