e7ea06899383aac31581baf7d8875abd81efbcab
misund <thomas@hemmeligadresse.com>
2017-11-24 00:33:06 +0100

    Apply outerMargin to non-fluid grids as well

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,11 +7,8 @@ import config, { DIMENSION_NAMES } from '../config'
 const Grid = styled.div`
   margin-right: auto;
   margin-left: auto;
-
-  ${p => p.fluid && css`
-    padding-right: ${p => config(p).outerMargin + 'rem'};
-    padding-left: ${p => config(p).outerMargin + 'rem'};
-  `}
+  padding-right: ${p => config(p).outerMargin + 'rem'};
+  padding-left: ${p => config(p).outerMargin + 'rem'};
 
   ${p => !p.fluid && css`
     ${DIMENSION_NAMES.map(t =>
