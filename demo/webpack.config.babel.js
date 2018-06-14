d6c3456c2bc41d9b4e49f73125308b452e15ce0c
Loïc Mahieu <mahieuloic@gmail.com>
2018-06-14 10:23:10 +0200

    webpack: make v4 friendly

--- demo/webpack.config.babel.js
+++ demo/webpack.config.babel.js
@@ -1,10 +1,9 @@
 
-import webpack from 'webpack'
 import path from 'path'
 
 module.exports = {
   context: __dirname,
-  devtool: 'inline-source-map',
+  mode: 'development',
   entry: './index.js',
   output: {
     path: path.join(__dirname, '/build'),
@@ -12,17 +11,14 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader'
+        use: 'babel-loader'
       }
     ]
   },
-  plugins: [
-    new webpack.NoErrorsPlugin()
-  ],
   devServer: {
     contentBase: __dirname
   }
