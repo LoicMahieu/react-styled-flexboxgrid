c13b9b5a4eb9c989da2b7c8f0561feafa9790648
Samuel Castro <samuelcastrosilva@gmail.com>
2020-06-08 15:06:42 -0700

    Including support for Emotion 10.x

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,12 @@
-
-const { NODE_ENV } = process.env
-const modules = NODE_ENV === 'production' ? false : 'commonjs'
-const loose = true
+const { NODE_ENV } = process.env;
+const modules = NODE_ENV === 'production' ? false : 'commonjs';
+const loose = true;
 
 module.exports = {
-  presets: [
-    ['@babel/env', { loose, modules }],
-    '@babel/react'
-  ],
+  presets: [['@babel/env', { loose, modules }], '@babel/react'],
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
-    ['@babel/plugin-proposal-class-properties', { loose }]
+    ['@babel/plugin-proposal-class-properties', { loose }],
   ],
   env: {
     emotion: {
@@ -20,12 +16,25 @@ module.exports = {
         [
           'transform-rename-import',
           {
-            original: '^styled-components$',
-            replacement: 'react-emotion'
-          }
+            replacements: [
+              {
+                original: "import { css } from 'styled-components'",
+                replacement: "import { css } from '@emotion/core'",
+              },
+              {
+                original: "import styled from 'styled-components'",
+                replacement: "import styled from '@emotion/styled'",
+              },
+              {
+                original: "import styled, { css } from 'styled-components'",
+                replacement:
+                  "import styled from '@emotion/styled'\nimport { css } from '@emotion/core'",
+              },
+            ],
+          },
         ],
-        'emotion'
-      ]
-    }
-  }
-}
+        'emotion',
+      ],
+    },
+  },
+};
