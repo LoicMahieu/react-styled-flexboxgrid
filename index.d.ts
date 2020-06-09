75d3ae3ce0acf0e5df7214e439d0d690b77441a6
Samuel Castro <samuelcastrosilva@gmail.com>
2020-06-08 17:22:17 -0700

    Removed styled-components dependencies on index.d.ts type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,67 @@
-declare module "react-styled-flexboxgrid" {
-  import { StyledComponent, DefaultTheme, ThemedCssFunction } from "styled-components";
+import { ThemedStyledProps } from 'styled-components';
+declare module '@samuelcastro/react-styled-flexboxgrid' {
+  interface ThemeProps<T> {
+    theme: T;
+  }
+
+  type ThemedStyledProps<P, T> = P & ThemeProps<T>;
+
+  type AnyIfEmpty<T extends object> = keyof T extends never ? any : T;
+
+  type FalseyValue = undefined | null | false;
+  type Interpolation<P> =
+    | InterpolationValue
+    | InterpolationFunction<P>
+    | FlattenInterpolation<P>;
+
+  // cannot be made a self-referential interface, breaks WithPropNested
+  // see https://github.com/microsoft/TypeScript/issues/34796
+  type FlattenInterpolation<P> = ReadonlyArray<Interpolation<P>>;
+
+  type InterpolationValue =
+    | string
+    | number
+    | FalseyValue
+    | Keyframes
+    | StyledComponentInterpolation
+    | CSSObject;
+
+  type SimpleInterpolation = InterpolationValue | FlattenSimpleInterpolation;
+
+  type FlattenSimpleInterpolation = ReadonlyArray<SimpleInterpolation>;
+
+  type InterpolationFunction<P> = (props: P) => Interpolation<P>;
+
+  type Attrs<P, A extends Partial<P>, T> =
+    | ((props: ThemedStyledProps<P, T>) => A)
+    | A;
+
+  interface BaseThemedCssFunction<T extends object> {
+    (
+      first: TemplateStringsArray | CSSObject,
+      ...interpolations: SimpleInterpolation[]
+    ): FlattenSimpleInterpolation;
+    (
+      first:
+        | TemplateStringsArray
+        | CSSObject
+        | InterpolationFunction<ThemedStyledProps<{}, T>>,
+      ...interpolations: Interpolation<ThemedStyledProps<{}, T>>[]
+    ): FlattenInterpolation<ThemedStyledProps<{}, T>>;
+    <P extends object>(
+      first:
+        | TemplateStringsArray
+        | CSSObject
+        | InterpolationFunction<ThemedStyledProps<P, T>>,
+      ...interpolations: Interpolation<ThemedStyledProps<P, T>>[]
+    ): FlattenInterpolation<ThemedStyledProps<P, T>>;
+  }
+
+  type ThemedCssFunction<T extends object> = BaseThemedCssFunction<
+    AnyIfEmpty<T>
+  >;
+
+  type DefaultTheme = {};
 
   namespace ReactStyledFlexboxgrid {
     interface ITheme {
@@ -50,26 +112,26 @@ declare module "react-styled-flexboxgrid" {
       };
     }
 
-    interface IGridProps {
+    interface IGridProps extends HTMLDivElement {
       fluid?: boolean;
     }
 
-    interface IRowProps {
+    interface IRowProps extends HTMLDivElement {
       reverse?: boolean;
-      start?: "xs" | "sm" | "md" | "lg";
-      center?: "xs" | "sm" | "md" | "lg";
-      end?: "xs" | "sm" | "md" | "lg";
-      top?: "xs" | "sm" | "md" | "lg";
-      middle?: "xs" | "sm" | "md" | "lg";
-      bottom?: "xs" | "sm" | "md" | "lg";
-      around?: "xs" | "sm" | "md" | "lg";
-      between?: "xs" | "sm" | "md" | "lg";
-      first?: "xs" | "sm" | "md" | "lg";
-      last?: "xs" | "sm" | "md" | "lg";
+      start?: 'xs' | 'sm' | 'md' | 'lg';
+      center?: 'xs' | 'sm' | 'md' | 'lg';
+      end?: 'xs' | 'sm' | 'md' | 'lg';
+      top?: 'xs' | 'sm' | 'md' | 'lg';
+      middle?: 'xs' | 'sm' | 'md' | 'lg';
+      bottom?: 'xs' | 'sm' | 'md' | 'lg';
+      around?: 'xs' | 'sm' | 'md' | 'lg';
+      between?: 'xs' | 'sm' | 'md' | 'lg';
+      first?: 'xs' | 'sm' | 'md' | 'lg';
+      last?: 'xs' | 'sm' | 'md' | 'lg';
       children?: React.ReactNode;
     }
 
-    interface IColProps {
+    interface IColProps extends HTMLDivElement {
       reverse?: boolean;
       xs?: boolean | number;
       sm?: boolean | number;
@@ -80,15 +142,16 @@ declare module "react-styled-flexboxgrid" {
       mdOffset?: number;
       lgOffset?: number;
     }
-
   }
 
   export type ITheme = ReactStyledFlexboxgrid.ITheme;
-  export const Grid: StyledComponent<"div", any, ReactStyledFlexboxgrid.IGridProps, never>;
-  export const Row: StyledComponent<"div", any, ReactStyledFlexboxgrid.IRowProps, never>;
-  export const Col: StyledComponent<"div", any, ReactStyledFlexboxgrid.IColProps, never>;
+  export const Grid: ReactStyledFlexboxgrid.IGridProps;
+  export const Row: ReactStyledFlexboxgrid.IRowProps;
+  export const Col: ReactStyledFlexboxgrid.IColProps;
 
-  export const config: (props: any) => {
+  export const config: (
+    props: any
+  ) => {
     media: {
       xs: ThemedCssFunction<DefaultTheme>;
       sm: ThemedCssFunction<DefaultTheme>;
